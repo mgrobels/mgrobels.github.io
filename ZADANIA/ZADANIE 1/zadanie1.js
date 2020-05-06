9a0fc4f1ae935c7408ec277b811a0a7b8d1e6caa
mgrobels <60349094+mgrobels@users.noreply.github.com>
2020-05-06 15:49:35 +0200

    Delete zadanie1.js

--- ZADANIA/ZADANIE 1/zadanie1.js
+++
@@ -1,8 +0,0 @@
-function sprawdz()
-{
-	var a = document.getElementById("pole").value;
-	if(a>0) document.getElementById("wynik").innerHTML = "LICZBA JEST DODATNIA";
-	else if(a<0) document.getElementById("wynik").innerHTML = "LICZBA JEST UJEMNA";
-	else if(a==0) document.getElementById("wynik").innerHTML = "LICZBA JEST RÓWNA 0";
-	else document.getElementById("wynik").innerHTML = "TO NIE JEST LICZBA";
-}
