31d0de0dff5feeb57facc391ebd99ab9f1954208
mgrobels <60349094+mgrobels@users.noreply.github.com>
2020-08-10 15:35:25 +0200

    v1.5

--- DjBonii/funkcje.js
+++ DjBonii/funkcje.js
@@ -31,24 +31,3 @@ document.getElementById("Bonii").addEventListener("click", function()
         duzyBonii=!duzyBonii;
     }
 });
-    const counters = document.querySelectorAll('.counter');
-    const speed = 200;
-    
-    counters.forEach(counter => {
-        const updateCount = () => {
-            const target = +counter.getAttribute('data-target');
-            const count = +counter.innerText;
-
-            const inc = target / speed;
-    
-            if (count < target) {
-
-                counter.innerText = Math.ceil(count + inc);
-                
-                setTimeout(updateCount, 50);
-            } else {
-                counter.innerText = target;
-            }
-        };
-        updateCount();
-    });
